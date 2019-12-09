806a7535e5026aac3fff4811a9e65763a6a4d2e9
stvkre <kiariemburuh@gmail.com>
2019-12-09 11:02:15 +0300

    Changed background image and adjusted UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,7 @@
-$(document).ready(function() {
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-      location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target ||
-        $('[name=' + this.hash.slice(1) + ']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-          .animate({
-            scrollTop: targetOffset
-          }, 1000);
-        return false;
-      }
-    }
-  });
-});
+const parallax = document.getElementById("main");
+window.addEventListener("scroll", function()
+{
+
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.05 + "px";
+})
